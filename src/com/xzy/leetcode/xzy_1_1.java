d2c6f5bfaef0d715a68b1553a61b30019b46e01c
xzy <2894702202@qq.com>
2024-04-14 09:55:24 +0800

    first commit

---
+++ src/com/xzy/leetcode/xzy_1_1.java
@@ -0,0 +1,29 @@
+package com.xzy.leetcode;
+
+import java.util.Arrays;
+
+public class xzy_1_1 {
+    public static void main(String[] args) {
+        int[] arr = {3,3};
+        int num = 6;
+        int[] ints = twoSum(arr, num);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    //双指针
+    public static int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+
+        //快指针
+        for (int i = 0; i < nums.length; i++) {
+            //慢指针
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
+        return res;
+    }
+}
